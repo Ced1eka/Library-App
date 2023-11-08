5f95e93817658f382455903321e3d3e5761c3207
Ced <cedieka2@gmail.com>
2023-11-08 11:19:28 +0900

    Finally added a button.

--- theCode.js
+++ theCode.js
@@ -8,19 +8,70 @@ function Book(title, author, pages, status) {
     return `${title} by ${author}, ${pages} pages, ${status}`;
   };
 }
+const myLibrary = [];
 
 const lotr = new Book("The Hobbit", "J.R.R Tolkein", 295, "not read");
+const lotr2 = new Book("The Hobbit", "J.R.R Tolkein", 295, "not read");
+
+myLibrary.push(lotr);
+myLibrary.push(lotr2);
 
 
-
-
-const myLibrary = [];
 
 function addBook(){
 
-myLibrary.push(lotr);
+myLibrary.forEach((item) =>{
+
+const container = document.querySelector(".container");
+
+// Create a new div element.
+const card = document.createElement("div");
+
+// Set the class attribute of the div element to "card grid".
+card.classList.add("card");
+card.classList.add("grid");
+
+// Create a new paragraph element for the title.
+const title = document.createElement("p");
+title.classList.add("title");
+title.textContent = item.title;
+
+// Create a new paragraph element for the author.
+const author = document.createElement("p");
+author.classList.add("author");
+author.textContent = item.author;
+
+// Create a new paragraph element for the pages.
+const pages = document.createElement("p");
+pages.classList.add("pages");
+pages.textContent = item.pages;
+
+// Create a new paragraph element for the status.
+const status = document.createElement("p");
+status.classList.add("status");
+status.textContent = item.status;
+
+// Append the paragraph elements to the div element.
+card.appendChild(title);
+card.appendChild(author);
+card.appendChild(pages);
+card.appendChild(status);
+
+// Append the div element to the body tag element.
+container.appendChild(card);
+
+});
+}
+addBook();
+
+const contain = document.querySelector("body");
+const btn = document.createElement("button");
+btn.classList.add("btn");
+btn.innerHTML = "Add Book"
+contain.appendChild(btn);
+
+
+function addNew(){
+
 
 }
-
-addBook();
-console.table(myLibrary)

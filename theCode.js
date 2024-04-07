a3d83b198875e35edb9fbea2a80914b62abff1a6
Ced <cedieka2@gmail.com>
2024-04-07 18:23:43 +0000

    refactored into class

--- theCode.js
+++ theCode.js
@@ -10,16 +10,21 @@ class Book {
   }
 }
 
-const myLibrary = [];
+class Library{
 
-const tH = new Book("The Hobbit", "J.R.R Tolkein", 295, "not read");
-const lotr = new Book("The Lord of the Rings", "J.R.R Tolkein", 1216, "read");
+  constructor(){
+    this.books = [];
 
-myLibrary.push(tH);
-myLibrary.push(lotr);
+  }
 
-function addBook() {
-  myLibrary.forEach((item, index) => {
+  addBook(book){
+    this.books.push(book)
+  }
+
+  addNew() {
+    //this holds reference to the last latest new book
+    const aNewBook = this.books[this.books.length - 1];
+
     const container = document.querySelector(".container");
 
     // Create a new div element.
@@ -28,47 +33,56 @@ function addBook() {
     // Set the class attribute of the div element to "card grid".
     card.classList.add("card");
     card.classList.add("grid");
-    card.dataset.bookNumber = index;
+    card.dataset.bookNumber = this.books.length - 1;
 
     // Create a new paragraph element for the title.
     const title = document.createElement("p");
     title.classList.add("title");
-    title.textContent = item.title;
+    title.textContent = aNewBook.title;
 
     // Create a new paragraph element for the author.
     const author = document.createElement("p");
     author.classList.add("author");
-    author.textContent = item.author;
+    author.textContent = aNewBook.author;
 
     // Create a new paragraph element for the pages.
     const pages = document.createElement("p");
     pages.classList.add("pages");
-    pages.textContent = item.pages;
+    pages.textContent = aNewBook.pages;
 
     // Create a new select element for the status.
     const status = document.createElement("select");
     status.setAttribute("name", "status");
     status.setAttribute("id", "status");
-
     //add options as values
     const read = document.createElement("option");
-    read.value = "read";
+    read.value = "1";
     read.textContent = "read";
     const notRead = document.createElement("option");
-    notRead.value = "not read";
+    notRead.value = "2";
     notRead.textContent = "not read";
     status.appendChild(read);
     status.appendChild(notRead);
-
-    if (item.status === "not read") {
+    if (aNewBook.status === "not-read") {
       status.selectedIndex = 1;
     } else status.selectedIndex = 0;
+
+    //modify status
+    status.addEventListener("change", () => {
+      if (status.value === "1") {
+        status.selectedIndex = 0;
+        aNewBook.status = "read";
+      } else {
+        status.selectedIndex = 1;
+        aNewBook.status = "not read";
+      }
+      // console.table(myLibrary)
+    });
 
     //Add in a button element to delete the entire book
     const del = document.createElement("button");
     del.classList.add("delete");
-    del.dataset.bookNumber = index;
-
+    del.dataset.bookNumber = this.books.length - 1;
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     const svgString = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M320 320L192 192M192 320l128-128"/></svg>`;
     svg.innerHTML = svgString;
@@ -87,7 +101,7 @@ function addBook() {
     // add delete book function
     del.addEventListener("click", () => {
       const cardNum = card.dataset.bookNumber;
-      myLibrary.splice(cardNum, 1);
+      this.books.splice(cardNum, 1);
       // const par =  del.parentElement;
       const divNum = document.querySelector(`[data-book-number="${cardNum}"]`);
       divNum.remove();
@@ -95,9 +109,92 @@ function addBook() {
         divNum.dataset.bookNumber = index;
       });
     });
-  });
+  }
+
+  render() {
+    this.books.forEach((item, index) => {
+      const container = document.querySelector(".container");
+
+      // Create a new div element.
+      const card = document.createElement("div");
+
+      // Set the class attribute of the div element to "card grid".
+      card.classList.add("card");
+      card.classList.add("grid");
+      card.dataset.bookNumber = index;
+
+      // Create a new paragraph element for the title.
+      const title = document.createElement("p");
+      title.classList.add("title");
+      title.textContent = item.title;
+
+      // Create a new paragraph element for the author.
+      const author = document.createElement("p");
+      author.classList.add("author");
+      author.textContent = item.author;
+
+      // Create a new paragraph element for the pages.
+      const pages = document.createElement("p");
+      pages.classList.add("pages");
+      pages.textContent = item.pages;
+
+      // Create a new select element for the status.
+      const status = document.createElement("select");
+      status.setAttribute("name", "status");
+      status.setAttribute("id", "status");
+
+      //add options as values
+      const read = document.createElement("option");
+      read.value = "read";
+      read.textContent = "read";
+      const notRead = document.createElement("option");
+      notRead.value = "not read";
+      notRead.textContent = "not read";
+      status.appendChild(read);
+      status.appendChild(notRead);
+
+      if (item.status === "not read") {
+        status.selectedIndex = 1;
+      } else status.selectedIndex = 0;
+
+      //Add in a button element to delete the entire book
+      const del = document.createElement("button");
+      del.classList.add("delete");
+      del.dataset.bookNumber = index;
+
+      const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      const svgString = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M320 320L192 192M192 320l128-128"/></svg>`;
+      svg.innerHTML = svgString;
+      del.appendChild(svg);
+
+      // Append the paragraph elements to the div element.
+      card.appendChild(title);
+      card.appendChild(author);
+      card.appendChild(pages);
+      card.appendChild(status);
+      card.appendChild(del);
+
+      // Append the div element to the body tag element.
+      container.appendChild(card);
+
+      // add delete book function
+      del.addEventListener("click", () => {
+        const cardNum = card.dataset.bookNumber;
+        this.books.splice(cardNum, 1);
+        // const par =  del.parentElement;
+        const divNum = document.querySelector(`[data-book-number="${cardNum}"]`);
+        divNum.remove();
+        document.querySelectorAll(".card").forEach((divNum, index) => {
+          divNum.dataset.bookNumber = index;
+        });
+      });
+    });
+  }
+
+
+
 }
-addBook();
+
 
 //append a button to the body
 const contain = document.querySelector("body");
@@ -111,6 +208,7 @@ btn.addEventListener("click", () => {
   dia.showModal();
 });
 
+//submit btn does this
 const sub = document.querySelector(".sub");
 sub.addEventListener("click", () => {
   //closes modal
@@ -120,12 +218,14 @@ sub.addEventListener("click", () => {
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const pages = document.getElementById("pages").value;
-  const status = document.getElementById("mod-status");
-  const logg = status.options[status.selectedIndex].value;
-  const newBook = new Book(title, author, pages, logg);
+  const logg = document.getElementById("mod-status");
+  // const status = logg.options[logg.selectedIndex].value;
+  const status = logg.selectedIndex !== -1 ? logg.options[logg.selectedIndex].value : "not read";
+
+  const newBook = new Book(title, author, pages, status);
   // console.log(newBook);
-  myLibrary.push(newBook);
-  addNew();
+  myLibrary.addBook(newBook);
+  myLibrary.addNew();
 });
 
 const dia = document.querySelector("dialog");
@@ -137,94 +237,20 @@ dia.addEventListener("close", () => {
   const pages = document.getElementById("pages");
   pages.value = "";
   const status = document.getElementById("mod-status");
-  const logg = status.options[status.selectedIndex];
-  logg.value = "";
+  status.selectedIndex = 0;
 });
 
-function addNew() {
-  const aNewBook = myLibrary[myLibrary.length - 1];
-  const container = document.querySelector(".container");
+//creates the library
+const myLibrary = new Library();
 
-  // Create a new div element.
-  const card = document.createElement("div");
+//temp books just to populate space
+const tH = new Book("The Hobbit", "J.R.R Tolkein", 295, "not read");
+const lotr = new Book("The Lord of the Rings", "J.R.R Tolkein", 1216, "read");
+const lotr2 = new Book("some book", "Me", 68, "not read");
 
-  // Set the class attribute of the div element to "card grid".
-  card.classList.add("card");
-  card.classList.add("grid");
-  card.dataset.bookNumber = myLibrary.length - 1;
 
-  // Create a new paragraph element for the title.
-  const title = document.createElement("p");
-  title.classList.add("title");
-  title.textContent = aNewBook.title;
-
-  // Create a new paragraph element for the author.
-  const author = document.createElement("p");
-  author.classList.add("author");
-  author.textContent = aNewBook.author;
-
-  // Create a new paragraph element for the pages.
-  const pages = document.createElement("p");
-  pages.classList.add("pages");
-  pages.textContent = aNewBook.pages;
-
-  // Create a new select element for the status.
-  const status = document.createElement("select");
-  status.setAttribute("name", "status");
-  status.setAttribute("id", "status");
-  //add options as values
-  const read = document.createElement("option");
-  read.value = "1";
-  read.textContent = "read";
-  const notRead = document.createElement("option");
-  notRead.value = "2";
-  notRead.textContent = "not read";
-  status.appendChild(read);
-  status.appendChild(notRead);
-  if (aNewBook.status === "not-read") {
-    status.selectedIndex = 1;
-  } else status.selectedIndex = 0;
-
-  //modify status
-  status.addEventListener("change", () => {
-    if (status.value === "1") {
-      status.selectedIndex = 0;
-      aNewBook.status = "read";
-    } else {
-      status.selectedIndex = 1;
-      aNewBook.status = "not read";
-    }
-    // console.table(myLibrary)
-  });
-
-  //Add in a button element to delete the entire book
-  const del = document.createElement("button");
-  del.classList.add("delete");
-  del.dataset.bookNumber = myLibrary.length - 1;
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  const svgString = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M320 320L192 192M192 320l128-128"/></svg>`;
-  svg.innerHTML = svgString;
-  del.appendChild(svg);
-
-  // Append the paragraph elements to the div element.
-  card.appendChild(title);
-  card.appendChild(author);
-  card.appendChild(pages);
-  card.appendChild(status);
-  card.appendChild(del);
-
-  // Append the div element to the body tag element.
-  container.appendChild(card);
-
-  // add delete book function
-  del.addEventListener("click", () => {
-    const cardNum = card.dataset.bookNumber;
-    myLibrary.splice(cardNum, 1);
-    // const par =  del.parentElement;
-    const divNum = document.querySelector(`[data-book-number="${cardNum}"]`);
-    divNum.remove();
-    document.querySelectorAll(".card").forEach((divNum, index) => {
-      divNum.dataset.bookNumber = index;
-    });
-  });
-}
+//add books to library
+myLibrary.addBook(lotr);
+myLibrary.addBook(tH);
+myLibrary.addBook(lotr2);
+myLibrary.render()
